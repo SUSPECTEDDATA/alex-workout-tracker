50bc64090015c8e35e807ed7249cb87ac632a051
SUSPECTEDDATA <fastalex10@gmail.com>
2026-04-03 03:40:03 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,17 @@
-const CACHE = 'workout-v2';
+// Minimal service worker for Cloudflare Pages
+const CACHE = 'workout-v3';
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll([
-      'index.html',
-      'history.html',
-      'progress.html',
-      'plan.html',
-      'manifest.json'
-    ]))
+    caches.open(CACHE).then(cache => 
+      cache.addAll([
+        'index.html',
+        'history.html',
+        'progress.html',
+        'plan.html',
+        'manifest.json'
+      ])
+    )
   );
 });
 
